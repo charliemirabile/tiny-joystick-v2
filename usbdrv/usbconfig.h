60bbc2cc3352835574da7e4c7c5ef3fdd1940cc9
NAME <>
2021-03-28 20:44:04 -0400

    change name to MiniMIDI JOYstick

--- usbdrv/usbconfig.h
+++ usbdrv/usbconfig.h
@@ -144,7 +144,7 @@ extern void usbEventResetReady(void);
  * obdev's free shared VID/PID pair. See the file USBID-License.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'M', 'i', 'n', 'i', 'M', 'I', 'D', 'I', ' ', 'J', 'o', 'y', 's', 't', 'i', 'c', 'k'
+#define USB_CFG_DEVICE_NAME     'M', 'i', 'n', 'i', 'M', 'I', 'D', 'I', ' ', 'J', 'O', 'Y', 's', 't', 'i', 'c', 'k'
 #define USB_CFG_DEVICE_NAME_LEN 17
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USBID-License.txt before you assign a name if you
